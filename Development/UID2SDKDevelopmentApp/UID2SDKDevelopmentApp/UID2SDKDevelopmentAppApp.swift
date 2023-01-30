f1bfe577ee000f949fb4209298c2e16b602c6922
Brad Leege <brad.leege@thetradedesk.com>
2023-01-30 11:36:56 -0600

    Setting Up DevelopmentApp

---
+++ Development/UID2SDKDevelopmentApp/UID2SDKDevelopmentApp/UID2SDKDevelopmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UID2SDKDevelopmentAppApp.swift
+//  UID2SDKDevelopmentApp
+//
+//  Created by Brad Leege on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct UID2SDKDevelopmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
