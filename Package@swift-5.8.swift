28155222570ffa94422a4453dec472c014cec976
Dave Snabel-Caunt <dave.snabel-caunt@thetradedesk.com>
2024-04-23 15:46:24 +0100

    Enable strict concurrency warnings, upgrade project

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UID2",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "UID2",
+            targets: ["UID2"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "UID2",
+            dependencies: [],
+            resources: [
+                .copy("Properties/sdk_properties.plist"),
+                .copy("PrivacyInfo.xcprivacy")
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency=complete")
+            ]
+        ),
+        .testTarget(
+            name: "UID2Tests",
+            dependencies: ["UID2"],
+            resources: [
+                .copy("TestData")
+            ]),
+    ]
+)
